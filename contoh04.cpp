355f523606122bbc4d6d22284823702c4cd7f2b8
RizaldiAkhmad <32384084+RizaldiAkhmad@users.noreply.github.com>
2017-11-19 22:17:17 +0800

    Mengkonversikan nilai dari pembilang dan penyebut serta menambahkan rumus
    Rumus>untuk menentukan yg mana pembilang dan penyebut

--- contoh04.cpp
+++ contoh04.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
 
+
+bool ConvertToLowestTerms(int &pembilang, int &penyebut);
+bool ConvertToLowestTerms(int &pembilang, int &penyebut)
+{
+if (penyebut == 0)
+return false;
+
+int FPB = penyebut;
+if (FPB > pembilang)
+FPB = pembilang;
+while (((pembilang % FPB) != 0) ||
+((penyebut % FPB) != 0))
+{
+FPB--;
+}
+pembilang /= FPB;
+penyebut /= FPB;
+return true;
+}
+
 int main(){
 	
 }
