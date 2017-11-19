427bb39b2896c3c3a5d13dc2cca85dbafe029a8a
RizaldiAkhmad <32384084+RizaldiAkhmad@users.noreply.github.com>
2017-11-19 22:08:23 +0800

    Memasukkan output

--- contoh03.cpp
+++ contoh03.cpp
@@ -21,5 +21,11 @@ return mph;
 }
 
 int main(){
+	cout << "Kecepatan 05:30 adalah " << ConvertToMPH(5,30) << " MPH." << endl;
+	cout << "Kecepatan 09:20 adalah " << ConvertToMPH(9,20) << " MPH." << endl;
+	cout << "Kecepatan 10:00 adalah " << ConvertToMPH(10,0) << " MPH." << endl;
+	cout << "08  kph adalah " << ConvertToMPH(8) << " MPH." << endl;
+	cout << "15  kph adalah " << ConvertToMPH(15) << " MPH." << endl;
+	cout << "125 kph adalah " << ConvertToMPH(125) << " MPH." << endl << endl;
 	
 }
