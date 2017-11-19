26a77d41f519544c446046fd999efddbaf26141c
RizaldiAkhmad <32384084+RizaldiAkhmad@users.noreply.github.com>
2017-11-19 22:20:21 +0800

    hasil output dari menentukan nilai pembilang dan penyebut

--- contoh04.cpp
+++ contoh04.cpp
@@ -22,5 +22,28 @@ return true;
 }
 
 int main(){
-	
+	int pembilang, penyebut;
+	pembilang = 36;
+	penyebut = 6;
+	cout << pembilang << " / " << penyebut<< " = ";
+	ConvertToLowestTerms(pembilang, penyebut);
+	cout << pembilang << " / " << penyebut << endl;
+	pembilang = 43;
+	penyebut = 17;
+	cout << pembilang << " / " << penyebut<< " = ";
+	ConvertToLowestTerms(pembilang, penyebut);
+	cout << pembilang << " / " << penyebut << endl;
+	pembilang = 0;
+	penyebut = 0;
+	cout << pembilang << " / " << penyebut<< " = ";
+	ConvertToLowestTerms(pembilang, penyebut);
+	cout << pembilang << " / " << penyebut << endl;
+	pembilang = 165;
+	penyebut = 45;
+	cout << pembilang << " / " << penyebut<< " = ";
+	ConvertToLowestTerms(pembilang, penyebut);
+	cout << pembilang << " / " << penyebut << endl;
+	cout << "Ketik apa saja untuk keluar dari output." << endl;
+	char wait; cin >> wait;
+	return 0;
 }
