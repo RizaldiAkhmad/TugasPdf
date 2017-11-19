b08fffeebd5d6c214e30a9aad16ccf78d0dce171
RizaldiAkhmad <32384084+RizaldiAkhmad@users.noreply.github.com>
2017-11-19 22:07:17 +0800

    memasukkan variabel dan mengkonversi

--- contoh03.cpp
+++ contoh03.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
 using namespace std;
+
+double ConvertToMPH(int kecepatanMin, int kecepatanPersec);
+double ConvertToMPH(double kph);
+double ConvertToMPH(int kecepatanMin, int kecepatanPersec){
+
+int secspermile;
+double mph;
+secspermile = kecepatanMin * 60 + kecepatanPersec;
+mph = (1 / static_cast<double>(secspermile)) * 3600;
+return mph;
+}
+
+double ConvertToMPH(double kph){
+double mph;
+mph = kph / 1.61;
+return mph;
+}
 
 int main(){
 	
