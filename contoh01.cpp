edc668ca0119faeed4adfff7a38cc1b3563395c7
RizaldiAkhmad <32384084+RizaldiAkhmad@users.noreply.github.com>
2017-11-19 21:13:46 +0800

    menginput fungsi void

--- contoh01.cpp
+++ contoh01.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+void input(int& jam24, int&menit);
+void konversi(int*jam, char&AMPM);
+void output(int jam, int menit, char AMPM);
+
 int main(){
 	
 }
